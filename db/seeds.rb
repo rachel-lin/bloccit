04398940607ddb91dd06bb645fd4bfa9cf50934b
Rachel Lin <rachel.lin91@gmail.com>
2015-04-19 17:04:36 +0800

    associations-checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,36 @@
 require 'faker'
 
+# Create Users
+
+5.times do
+  user = User.new(
+    name:  Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10) 
+    )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 # Create Posts
 
 
 50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
     )
 end
 
-
-Post.find_or_create_by (
+=begin
+  Post.find_or_create_by (
   :title => "additional post 1",
   :body => "additional post 1 content")
+=end
+
 
 posts = Post.all 
 
@@ -25,11 +42,20 @@ posts = Post.all
     )  
 end
 
+=begin
 Comment.find_or_create_by (
   :body => "additional post 1 comment")
+=end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'rachel.lin91@gmail.com',
+  password: 'helloworld'
+  )
 
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
