ca8c4939f4acd4032fba67a647696995ecdd039e
Rachel Lin <rachel.lin91@gmail.com>
2015-04-30 16:41:44 +0800

    post-markdown attempt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,10 +49,27 @@ class PostsController < ApplicationController
       end
     end
 
+  def markdown_title
+    @post.title.render_as_markdown
+  end
+
+  def markdown_body
+    @post.body.render_as_markdown
+  end
+
+
   private
 
   def post_params
     params.require(:post).permit(:title, :body)
   end
+
+  def render_as_markdown
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 
 end
