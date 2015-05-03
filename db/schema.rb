de8ce767b4d68f8784d86638273fb72f453b6780
Rachel Lin <rachel.lin91@gmail.com>
2015-05-04 01:52:01 +0800

    added value column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502192643) do
+ActiveRecord::Schema.define(version: 20150503173739) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
