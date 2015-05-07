b8481155f4e074a9951e3c8cf97e702846b2a2c3
Rachel Lin <rachel.lin91@gmail.com>
2015-05-08 04:15:11 +0800

    used helper methods to shorten voter string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,14 @@ def markdown_to_html(markdown)
   (redcarpet.render markdown).html_safe
 end
 
+def up_vote_link_classes(post)
+  "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+end
+
+def down_vote_link_classes(post)
+  "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+end
+
+
 end
 
